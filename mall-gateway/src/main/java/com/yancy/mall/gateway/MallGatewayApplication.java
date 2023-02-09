6015fd961243cffef352f5c0ab2527b50f94b231
yancy0109 <1156729237@qq.com>
2023-02-09 18:49:35 +0800

    网关模块

---
+++ mall-gateway/src/main/java/com/yancy/mall/gateway/MallGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.yancy.mall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author yancy0109
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MallGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallGatewayApplication.class);
+    }
+}
